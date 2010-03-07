84ab684761b4b89bf69e4f59ee021efd3f9b7072
Chris Wanstrath <chris@ozmm.org>
2010-03-06 19:55:23 -0800

    comments

--- coffee-mode.el
+++ coffee-mode.el
@@ -1,18 +1,18 @@
 ;; JavaScript Keywords
-(setq coffee-js-keywords
+(defvar coffee-js-keywords
       '("if" "else" "true" "false" "new" "return" "try" "catch"
         "finally" "throw" "break" "continue" "for" "in" "while"
         "delete" "instanceof" "typeof" "switch" "super" "extends"
         "class"))
 
 ;; Reserved keywords either by JS or CS.
-(setq coffee-js-reserved
+(defvar coffee-js-reserved
       '("case" "default" "do" "function" "var" "void" "with"
         "const" "let" "debugger" "enum" "export" "import" "native"
         "__extends" "__hasProp"))
 
 ;; CoffeeScript keywords.
-(setq coffee-cs-keywords
+(defvar coffee-cs-keywords
       '("then" "unless" "yes" "no" "on" "off" "and" "or" "is"
         "isnt" "not" "of" "by" "where" "when"))
 
@@ -25,7 +25,7 @@
 (setq coffee-types '(""))
 (defvar coffee-type-regexp (regexp-opt coffee-types 'words))
 
-(setq coffee-constant-regexp "")
+(defvar coffee-constant-regexp "")
 
 (setq coffee-events '(""))
 (defvar coffee-event-regexp (regexp-opt coffee-events 'words))
@@ -35,7 +35,7 @@
 
 ;; create the list for font-lock.
 ;; each class of keyword is given a particular face
-(setq coffee-font-lock-keywords
+(defvar coffee-font-lock-keywords
       `(
         (,coffee-type-regexp . font-lock-type-face)
         (,coffee-constant-regexp . font-lock-constant-face)
@@ -48,6 +48,9 @@
         ;; would be highlighted.
         ))
 
+
+
+
 ;; define the mode
 (define-derived-mode coffee-mode fundamental-mode
   "coffee-mode"
@@ -55,6 +58,13 @@
 
   ;; code for syntax highlighting
   (setq font-lock-defaults '((coffee-font-lock-keywords)))
+
+  ;; modify the keymap
+  (define-key coffee-mode-map [remap comment-dwim] 'coffee-comment-dwim)
+
+  ;; perl style comment: "# ..."
+  (modify-syntax-entry ?# "< b" coffee-mode-syntax-table)
+  (modify-syntax-entry ?\n "> b" coffee-mode-syntax-table)
 
   ;; clear memory
   (setq coffee-keywords-regexp nil)
