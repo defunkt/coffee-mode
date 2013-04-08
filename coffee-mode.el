e2e940ab2594c474c006aee0c2360e27c45813d5
William Bert <william@spanishdict.com>
2013-04-08 09:51:07 -0400

    No highlighting regexps.
    https://github.com/defunkt/coffee-mode/issues/110 needs to be fixed.

--- coffee-mode.el
+++ coffee-mode.el
@@ -415,8 +415,9 @@ called `coffee-compiled-buffer-name'."
 (defvar coffee-boolean-regexp "\\b\\(true\\|false\\|yes\\|no\\|on\\|off\\|null\\|undefined\\)\\b")
 
 ;; Regular Expressions
-(defvar coffee-regexp-regexp "\\/\\(\\\\.\\|\\[\\(\\\\.\\|.\\)+?\\]\\|[^/
-]\\)+?\\/")
+;; Ignore this until https://github.com/defunkt/coffee-mode/issues/110 is fixed.
+;(defvar coffee-regexp-regexp "\\/\\(\\\\.\\|\\[\\(\\\\.\\|.\\)+?\\]\\|[^/
+;]\\)+?\\/")
 
 ;; JavaScript Keywords
 (defvar coffee-js-keywords
