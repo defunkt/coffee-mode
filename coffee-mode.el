bc8b8e9e0ba144ff4808ae38f139de5a723ad967
Syohei YOSHIDA <syohex@gmail.com>
2014-05-19 23:22:22 +0900

    Update version for MELPA-stable(#238)

--- coffee-mode.el
+++ coffee-mode.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2010 Chris Wanstrath
 
-;; Version: 0.5.0
+;; Version: 0.5.1
 ;; Keywords: CoffeeScript major mode
 ;; Author: Chris Wanstrath <chris@ozmm.org>
 ;; URL: http://github.com/defunkt/coffee-mode
