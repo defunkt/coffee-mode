4cf959a24b701acf7f1c9145d8fa9660c54c9fda
Syohei YOSHIDA <syohex@gmail.com>
2014-08-06 15:37:09 +0900

    Bump up version and update Changes

--- coffee-mode.el
+++ coffee-mode.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2010 Chris Wanstrath
 
-;; Version: 0.5.4
+;; Version: 0.5.5
 ;; Keywords: CoffeeScript major mode
 ;; Author: Chris Wanstrath <chris@ozmm.org>
 ;; URL: http://github.com/defunkt/coffee-mode
@@ -137,7 +137,7 @@
 ;; Customizable Variables
 ;;
 
-(defconst coffee-mode-version "0.5.4"
+(defconst coffee-mode-version "0.5.5"
   "The version of `coffee-mode'.")
 
 (defgroup coffee nil
