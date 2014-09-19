5aea8fa851b0cd9907745ac511ecde7eb147432c
Syohei YOSHIDA <syohex@gmail.com>
2014-09-19 16:08:41 +0900

    Update Copyright year

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,6 +1,6 @@
 ;;; test-helper.el --- helper for testing coffee-mode
 
-;; Copyright (C) 2013 by Syohei YOSHIDA
+;; Copyright (C) 2014 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 
